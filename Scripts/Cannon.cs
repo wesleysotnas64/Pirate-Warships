96f57b0d7318c58df64f7e318d61bec977c8ba31
Wesley Santos <wesleysotnas64@gmail.com>
2023-02-19 18:11:06 -0300

    :sparkles: feat: Som do canhão adicionado.

--- Scripts/Cannon.cs
+++ Scripts/Cannon.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
-    public GameObject ProjectilPrefab;
+    public GameObject  ProjectilPrefab { get; set; }
+    public AudioSource ShotSound { get; set; }
 
     private void Awake()
     {
         ProjectilPrefab = Resources.Load<GameObject>("Projectile");
+        ShotSound = GetComponent<AudioSource>();
+        ShotSound.volume = 0.025f;
     }
 
     public void Shoot()
     {
         Instantiate(ProjectilPrefab, transform.position, transform.rotation);
+        ShotSound.Play(0);
     }
 }
