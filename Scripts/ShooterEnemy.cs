d8717840a6ab352654ff98856ea6b97c828b823d
Wesley Santos <wesleysotnas64@gmail.com>
2023-02-20 13:32:45 -0300

    :sparkles: feat: ShooterEnemy implementado.

---
+++ Scripts/ShooterEnemy.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class ShooterEnemy : DefaultEnemy
+{
+    [SerializeField]
+    private float shootingRadius;
+    private void Start() {
+        maxLife = 5;
+        currentLife = maxLife;
+        speed = 0.75f;
+        curveRadius = 45;
+
+        lifeBar = Instantiate(Resources.Load<GameObject>("LifeBar"));
+        lifeBar.GetComponent<LifeBar>().ShipTransform = GetComponent<Transform>();
+
+        sensors = new List<Sensor>();
+        for (int i = 0; i < 2; i++)
+        {
+            Sensor sensor = gameObject.transform.GetChild(i).gameObject.GetComponent<Sensor>();
+            sensors.Add(sensor);
+        }
+
+        cannons = new List<GameObject>();
+        GameObject cannon = gameObject.transform.GetChild(2).gameObject;
+        cannons.Add(cannon);
+    }
+
+    void Update()
+    {
+        if (!OnTarget())
+            Navigate();
+        else
+            cannons[0].GetComponent<Cannon>().Shoot();
+        UpdateDirection();
+    }
+
+    private bool OnTarget()
+    {
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+        float distance = Vector3.Distance(playerTransform.position, transform.position);
+
+        return distance < shootingRadius ? true : false;
+    }
+
+}
